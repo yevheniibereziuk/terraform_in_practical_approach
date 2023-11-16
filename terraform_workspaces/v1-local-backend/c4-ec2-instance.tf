ae7c29f207486864383529ab1796d8384ba75c3e
Yevhenii Bereziuk <zgeka771@gmail.com>
2023-11-16 22:20:57 +0200

    chore add screenshots

--- terraform_workspaces/v1-local-backend/c4-ec2-instance.tf
+++ terraform_workspaces/v1-local-backend/c4-ec2-instance.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "my-ec2-vm" {
   ami           = data.aws_ami.amzlinux.id 
   instance_type = var.instance_type
 	user_data = file("apache-install.sh")  
-  count = terraform.workspace == "defaut" ? 2 : 1
+  count = terraform.workspace == "default" ? 2 : 1
   vpc_security_group_ids = [aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id]
   tags = {
     "Name" = "vm-${terraform.workspace}-${count.index}"
