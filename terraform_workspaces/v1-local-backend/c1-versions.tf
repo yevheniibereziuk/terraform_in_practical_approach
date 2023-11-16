79496045d2793b8da126a3658375a1e6889d0d8a
Yevhenii Bereziuk <zgeka771@gmail.com>
2023-11-16 21:27:58 +0200

    chore: add dirs with terraform workspaces

---
+++ terraform_workspaces/v1-local-backend/c1-versions.tf
@@ -0,0 +1,29 @@
+# Terraform Block
+terraform {
+  required_version = ">= 1.4" 
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+  # Adding Backend as S3 for Remote State Storage
+  backend "s3" {
+    bucket = "terraform-bereziuk"
+    key    = "statecommands/terraform.tfstate"
+    region = "us-east-1" 
+    # For State Locking
+    #dynamodb_table = "terraform-dev-state-table"    
+    
+  }
+}
+
+# Provider Block
+provider "aws" {
+  region  = var.aws_region
+  profile = "default"
+}
+/*
+Note-1:  AWS Credentials Profile (profile = "default") configured on my  local desktop terminal  
+$HOME/.aws/credentials
+*/
---
+++ terraform_workspaces/v1-local-backend/c4-ec2-instance.tf
@@ -0,0 +1,23 @@
+# Create EC2 Instance - Amazon Linux
+resource "aws_instance" "my-ec2-vm" {
+  ami           = data.aws_ami.amzlinux.id 
+  instance_type = var.instance_type
+	user_data = file("apache-install.sh")  
+  count = terraform.workspace == "defaut" ? 2 : 1
+  vpc_security_group_ids = [aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id]
+  tags = {
+    "Name" = "vm-${terraform.workspace}-${count.index}"
+  }
+}
+
+# resource "aws_instance" "my-demo-vm" {
+#   ami = data.aws_ami.amzlinux.id
+#   instance_type = var.instance_type
+#   tags = {
+#     "Name" = "demo-vm1"
+#   }
+# }
+
+
+
+
