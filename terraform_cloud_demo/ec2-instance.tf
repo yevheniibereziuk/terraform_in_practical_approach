6563b830534e25e4534017da9c46572d4b164e5f
Yevhenii Bereziuk <zgeka771@gmail.com>
2023-11-30 21:10:34 +0200

    change count of instances

--- terraform_cloud_demo/ec2-instance.tf
+++ terraform_cloud_demo/ec2-instance.tf
@@ -1,20 +1,13 @@
 # Create EC2 Instance - Amazon Linux
 resource "aws_instance" "my-ec2-vm" {
   ami           = data.aws_ami.amzlinux.id 
+  count = 2
   instance_type = var.instance_type
 	user_data = file("apache-install.sh")  
   vpc_security_group_ids = [aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id]
   tags = {
     "Name" = "amz-linux-vm"
     "target" = "Target-Test-1"
-  }
-}
-
-resource "aws_instance" "my-demo-vm" {
-  ami = data.aws_ami.amzlinux.id
-  instance_type = var.instance_type
-  tags = {
-    "Name" = "demo-vm1"
   }
 }
 
